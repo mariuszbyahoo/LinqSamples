1e13189101b51a4388ca4105cd7b4bd7db5a3915
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-02 09:05:05 +0200

    LINQ and C# : Using Func and Action Types

--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -11,6 +11,16 @@ namespace LinqSamples
     {
         static void Main(string[] args)
         {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3, 5))); // 8^2 = 64
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Scott" },
@@ -22,13 +32,12 @@ namespace LinqSamples
                 new Employee {Id = 3, Name = "Alex"}
             };
 
-            foreach (var employee in developers.Where
-                (e => e.Name.StartsWith("S")))
+            foreach (var employee in developers.Where(e => e.Name.Length == 5)
+                                               .OrderBy(e => e.Name))
             {
                 Console.WriteLine(employee.Name);
             }
         }
-
         private static bool NameStartsWithS(Employee employee)
         {
             return employee.Name.StartsWith("S");

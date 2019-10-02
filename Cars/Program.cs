6588cc7d72d68574a701dfaf645d51ec603d427a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-02 13:13:55 +0200

    Filter, Ordering & Projecting: Projecting Data with Select

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -23,39 +23,72 @@ namespace Cars
             var result = cars.Any(c => c.Manufacturer == "Ford"); // Returns True/False
             var result2 = cars.All(c => c.Manufacturer == "Ford"); // Returns True/False
 
-
             Console.WriteLine(result);
             Console.WriteLine(result2);
 
             //Query syntax
             var query2 = from car in cars
-                         where car.Manufacturer == "BMW"  && car.Year == 2016
+                         where car.Manufacturer == "BMW" && car.Year == 2016
                          orderby car.Combined descending, car.Name ascending
-                         select car;
+                         select new
+                         {
+                             car.Manufacturer,
+                             car.Name,
+                             car.Combined
+                         };
+            // Select statement above uses shortened syntax and returns a new anonymous type
+            // object; good for working with files which contains a plenty of columns to 
+            // deal with
+
+            var result3 = cars.Select(c => new { c.Manufacturer, c.Name, c.Combined });
+            // result3 above products the same result as the select statement from line 33
 
             foreach (var car in query2.Take(10))
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
             }
         }
 
         private static List<Car> ProcessFile(string path)
         {
             // Query syntax:
-            var query =
-                from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
-            
+            //var query =
+            //    from line in File.ReadAllLines(path).Skip(1)
+            //    where line.Length > 1
+            //    select Car.ParseFromCsv(line);
+
             // Extension method syntax
             var query2 =
                 File.ReadAllLines(path)
                     .Skip(1)
                     .Where(line => line.Length > 1)
-                    .Select(Car.ParseFromCsv)
-                    .ToList();
+                    .ToCar();
 
-            return query.ToList();
+            return query2.ToList();
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car // without yield statement the code will be trying to 
+                // return a first new Car obj and it generates a compiler error
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
         }
     }
 }

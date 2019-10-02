feb44ef88546f2f0b583ee496873340ce0798d03
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-02 10:32:03 +0200

    LINQ Queries: Querying Infinity

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -8,6 +8,13 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
             var movies = new List<Movie>
             {
                 new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2010 },
@@ -15,8 +22,11 @@ namespace Queries
                 new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1995 },
                 new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1990 }
             };
-            var query = movies.Where(m => m.Year > 2000)
-                                           .OrderByDescending(m => m.Rating);
+            var query = from movie in movies
+                        where movie.Year > 2000
+                        orderby movie.Rating descending
+                        select movie;
+
             var enumerator = query.GetEnumerator();
             while (enumerator.MoveNext())
             {

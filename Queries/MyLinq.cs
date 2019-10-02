a259182e54b96cb1fcd7f6956886d07789da69c5
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-02 09:43:53 +0200

    LINQ Queries: Creating an Operator with Yield Return

--- Queries/MyLinq.cs
+++ Queries/MyLinq.cs
@@ -9,18 +9,16 @@ namespace Queries
         public static IEnumerable<T> Filter<T> (this IEnumerable<T> source,
                                                 Func<T, bool> predicate)
         {
-            var result = new List<T>();
-
             foreach (var item in source)
             {
                 if (predicate(item))
                 {
-                    result.Add(item);
+                    yield return item; // Short desc on yield keyword:
+                    // It will help me to build an IEnumerable
+                    // In other words: every time I use an yield return syntax, 
+                    // my return type has to be an IEnumerable or IEnumerable<T>
                 }
-
             }
-
-            return result;
         }
     }
 }

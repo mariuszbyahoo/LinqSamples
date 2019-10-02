c3a213cd4ef786f50bb9aa5270b0248528c96549
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-02 12:14:27 +0200

    Filter, Ordering & Projecting: Implementing a File Processor

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace Cars
 {
     class Program
     {
+        // CSVs moved to ../Bin/Debug/netcoreapp 3.0/..
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Changing the Thread's culture Needed because of the CSV's format
+            CultureInfo culture = new CultureInfo("en-US"); 
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            var cars = ProcessFile("fuel.csv");
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            // Query syntax:
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+            
+            // Extension method syntax
+            var query2 =
+                File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv)
+                .ToList();
+
+            return query.ToList();
         }
     }
 }

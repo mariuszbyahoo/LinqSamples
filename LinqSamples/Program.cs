f3db489a905694c30b0e61240e0e9adefcd65d13
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-02 09:10:39 +0200

    LINQ and C#: Using var for Implicit Typing

--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -21,19 +21,21 @@ namespace LinqSamples
 
             write(square(add(3, 5))); // 8^2 = 64
 
-            IEnumerable<Employee> developers = new Employee[]
+            var developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Scott" },
                 new Employee {Id = 2, Name = "Chris" }
             };
 
-            IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee {Id = 3, Name = "Alex"}
             };
 
-            foreach (var employee in developers.Where(e => e.Name.Length == 5)
-                                               .OrderBy(e => e.Name))
+            var query = developers.Where(e => e.Name.Length == 5)
+                                               .OrderBy(e => e.Name);
+
+            foreach (var employee in query)
             {
                 Console.WriteLine(employee.Name);
             }
